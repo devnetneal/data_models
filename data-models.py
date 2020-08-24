2ef2a46eda7cc67257a5dd737ba22b6e892b8797
nmyersco@cisco.com <nmyersco@cisco.com>
2020-08-24 10:58:39 +0100

    adding changes to xml

--- data-models.py
+++ data-models.py
@@ -21,6 +21,7 @@ print(json_dict['Family']['Myerscough'][0]['Name'])
 
 Second is to open XML and put into dictionary. XML minidom topretty xml is used to display the XML nicely
 Should return 'My Name is Neal'
+updated
 
 """
 
@@ -33,7 +34,7 @@ xml_dict = xmltodict.parse(xml_data)
 #
 # print(x)
 
-# pprint(xml_dict)
+pprint(xml_dict)
 
 print('My name is {}'.format(xml_dict['Family']['Myerscough']['Member'][0]['Name']))
 
